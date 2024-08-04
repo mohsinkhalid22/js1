33df1a06a955247432bab7777cef41a0960c003e
mohsinkhalid22 <mohsinu2gud@gmail.com>
2024-08-04 13:20:11 +0500

    Initial commit

---
+++ neu.js
@@ -0,0 +1,57 @@
+fullName = "Tony Stark"
+age = 24 
+price = 90.99
+x = null
+y = undefined
+isFollow = false
+let totalPrice = 1000
+let a = BigInt("123");
+let b = Symbol("Helloo");
+
+const student = {
+    fullName : "Sam Ali",
+    age : 23,
+    cgpa: 3.3,
+    isPass: true,
+
+};
+
+student["age"] = student["age"] +1;
+
+const product = {
+    fullName : "Parker Jotter Standard CT Ball Pen(Black)",
+    rating: 4,
+    offer: 5,
+    Price: 500,
+};
+
+const profile = {
+    userName : "@jonadams",
+    followers: 1050,
+    following: 5050,
+    posts: 150,
+    isFollow: false,
+};
+
+
+
+
+
+
+
+
+console.log(profile)
+console.log(typeof profile["isFollow"])
+
+console.log(product)
+
+console.log(student)
+console.log(student["age"])
+console.log(student["cgpa"])
+console.log(fullName)
+console.log(age)
+console.log(price)
+console.log(x)
+console.log(y)
+console.log(isFollow)
+console.log(totalPrice)
